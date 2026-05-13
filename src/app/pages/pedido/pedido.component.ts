3cd18cdc137ee6cd31e22a8178c77389561afc6a
Luis Contreras <luccinos.64@gmail.com>
2026-05-13 14:56:29 -0400

    Validaciones de campos con bootstrap, mensajes de alerta reemplazados y adición de mensaje tipo toast en grabación de pedido

--- src/app/pages/pedido/pedido.component.ts
+++ src/app/pages/pedido/pedido.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+declare const bootstrap: any;
 
 @Component({
   selector: 'app-pedido',
@@ -14,14 +17,26 @@ export class PedidoComponent implements OnInit {
   cantidad: number | null = null;
   pedidosRealizados: any[] = [];
 
+
+  constructor(private router: Router) { }
+
   ngOnInit() {
+
     const guardados = localStorage.getItem('pedidos_comida');
+    const usuario = localStorage.getItem('usuario-logeado'); //Se revisa que el usuario haya hecho login desde el inicio component
+
+    if (!usuario || usuario === 'false') { //Si el usuario no existe o no hizo el login correctamente, se vuelve al inicio de forma automática
+      this.router.navigate(['/']);
+      return;
+    }
+
     if (guardados) {
       this.pedidosRealizados = JSON.parse(guardados);
     }
+    console.log(this.router);
   }
 
-  registrarPedido(form: any) {
+  registrarPedido(form: any) { //Para guardar pedidos en el almacenamiento local del navegador
     if (form.invalid) return;
 
     const nuevoPedido = {
@@ -32,8 +47,11 @@ export class PedidoComponent implements OnInit {
 
     this.pedidosRealizados.push(nuevoPedido);
     localStorage.setItem('pedidos_comida', JSON.stringify(this.pedidosRealizados));
-    
+
     form.reset();
-    alert('¡Pedido registrado con éxito!');
+    // alert('¡Pedido registrado con éxito!');
+    const toastLiveExample = document.getElementById('liveToast');
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+    toastBootstrap.show();
   }
 }
